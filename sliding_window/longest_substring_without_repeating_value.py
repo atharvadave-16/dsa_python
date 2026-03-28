e47e2dd08927ab3c11f92d7a8a045ba1d3eeaaa4
Atharva Dave <atharva.25bai10301@vitbhopal.ac.in>
2026-03-28 18:47:33 +0530

    learned optimal solution by sliding window for longest substring without repetiiton

--- sliding_window/longest_substring_without_repeating_value.py
+++ sliding_window/longest_substring_without_repeating_value.py
@@ -15,3 +15,17 @@ def lengthOfLongestSubstring(self, s: str) -> int:
                 value = len(a) 
         if stack: return(max(stack)) 
         else: return 0   
+
+#brute force applied first learned concept sliding window 
+# below is the correct and efficient structure, used set for o(1) per operation 
+def lengthOfLongestSubstring(self, s: str) -> int:
+        set1 = set()
+        length  = 0
+        l = 0
+        for r in range(len(s)):
+            while s[r] in set1:
+                set1.remove(s[l])
+                l = l + 1
+            set1.add(s[r])
+            length = max(length,r-l+1)    
+        return(length)         
