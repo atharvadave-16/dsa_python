f44b42acdfa463d3b0b3de323df0dfbd29e2da9d
Atharva Dave <atharva.25bai10301@vitbhopal.ac.in>
2026-04-02 23:27:06 +0530

    floyd tortoise and hare method for linked list cycle

--- linked_list/linked_list_cycle.py
+++ linked_list/linked_list_cycle.py
@@ -16,3 +16,12 @@ def hasCycle(self, head: Optional[ListNode]) -> bool:
 
 
 # first approach with hashmap
+#floyd tortoise and hare
+def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow,fast =head,head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False 
