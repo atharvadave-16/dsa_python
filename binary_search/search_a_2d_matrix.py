d97d3194a0708ad654ea10125307c0f6ceb4fdf1
Atharva Dave <atharva.25bai10301@vitbhopal.ac.in>
2026-04-15 22:50:48 +0530

    binary approach for 2d matrix

--- binary_search/search_a_2d_matrix.py
+++ binary_search/search_a_2d_matrix.py
@@ -12,3 +12,26 @@ def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         return False
 
 #first approach
+
+#binary approach
+
+def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n -1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if self.get(matrix, mid) == target:
+                return True
+            elif self.get(matrix, mid) < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
+
+       
+
+
+def get(self, mat, index):
+        m ,n = len(mat), len(mat[0])
+        i, j = divmod(index, n)
+        return mat[i][j]
