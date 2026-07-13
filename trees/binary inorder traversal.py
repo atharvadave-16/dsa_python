49cbcab5770f90f3386258fd3ed7c7cf380fdee0
Atharva Dave <atharva.25bai10301@vitbhopal.ac.in>
2026-07-13 23:17:59 +0530

    binary inorder traversal

---
+++ trees/binary inorder traversal.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val 
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        a =  []
+        def iot(node):
+         if not node:
+             return
+         iot(node.left)
+         a.append(node.val)
+         iot(node.right)
+        iot(root)
+        return a 
