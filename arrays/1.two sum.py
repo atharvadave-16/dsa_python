35559039ef94c2849e44cc8a06963b9153f8b348
Atharva Dave <atharva.25bai10301@vitbhopal.ac.in>
2026-07-23 11:05:09 +0530

    two sum optimal solution

--- arrays/1.two sum.py
+++ arrays/1.two sum.py
@@ -4,3 +4,19 @@ for i in range (0,len(nums)-1):
     for j in range (i + 1, len(nums)):
         if nums[i] + nums[j] == target:
             print([i,j])
+
+
+# Pattern: HashMap
+# Approach: for each num check if complement exists in map
+# Key trick: store AFTER checking to avoid same index twice
+# TC: O(n) | SC: O(n)
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+     
+     a ={}
+     for i in range(len(nums)):
+        num = nums[i]
+        b = target - num
+        if b in a:
+          return [a[b], i]
+        a[num] = i
