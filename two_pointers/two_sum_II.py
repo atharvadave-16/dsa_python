dcde00245d3cafa76c9920a71e1949d74a7a0133
Atharva Dave <atharva.25bai10301@vitbhopal.ac.in>
2026-07-28 22:07:30 +0530

    two sum II revise

--- two_pointers/two_sum_II.py
+++ two_pointers/two_sum_II.py
@@ -1,10 +1,13 @@
-def twoSum(self, numbers: list[int], target: int) -> list[int]:
-        p1 = 0
-        p2 = len(numbers) - 1
-        while numbers[p1] + numbers[p2] != target:
-            if numbers[p1] + numbers[p2] > target:
-                p2 = p2-1
-            else:p1 = p1+1    
-        return [p1+1,p2+1]
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+       a = 0
+       b = len(numbers) - 1
+       c = numbers[a] + numbers[b]
+       while c != target:
+        if c > target:
+            b = b-1
+        if c < target:
+            a = a + 1
+       return [a+1,b+1]
 
-# two avoid cases with no solution we can use while p1<p2:
+# two avoid cases with no solution we can use while a<b:
