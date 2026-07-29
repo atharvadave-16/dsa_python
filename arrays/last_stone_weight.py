80d9fbf9245bde423b9121c926f6c8ce164504dd
Atharva Dave <atharva.25bai10301@vitbhopal.ac.in>
2026-07-29 12:56:14 +0530

    last stone weight heap approach

--- arrays/last_stone_weight.py
+++ arrays/last_stone_weight.py
@@ -1,15 +1,12 @@
-def lastStoneWeight(self, stones):
-        """
-        :type stones: List[int]
-        :rtype: int
-        """
-        while (len(stones) > 1):
-            y = max(stones)
-            stones.remove(y)
-            x = max(stones)
-            stones.remove(x)
-            if x != y:
-                stones.append(y-x)
-        if stones :        
-         return stones[0]        
-        else: return 0 
+import heapq
+class Solution:
+    def lastStoneWeight(self, stones: list[int]) -> int:
+       a = [-i for i in stones]
+       heapq.heapify(a)
+       while len(a) > 1:
+        x = -heapq.heappop(a)
+        y = -heapq.heappop(a)
+        if   x != y :
+            heapq.heappush(a, -(x-y))
+
+       return -a[0] if a else 0        
